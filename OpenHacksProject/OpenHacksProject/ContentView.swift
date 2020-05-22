8806812b2d2ee5f088ed6fa47592a26eb5ce8175
RahulR30 <55637485+RahulR30@users.noreply.github.com>
2020-05-22 13:47:04 -0700

    Add files via upload

---
+++ OpenHacksProject/OpenHacksProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OpenHacksProject
+//
+//  Created by Rahul Rao on 5/22/20.
+//  Copyright © 2020 Rahul Rao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
