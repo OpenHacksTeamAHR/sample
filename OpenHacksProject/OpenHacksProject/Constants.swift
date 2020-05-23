041e0ca94f4fb670811efeefe1536ecb4a826352
RahulR30 <55637485+RahulR30@users.noreply.github.com>
2020-05-23 12:33:42 -0700

    Add files via upload

---
+++ OpenHacksProject/OpenHacksProject/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  OpenHacksProject
+//
+//  Created by Rahul Rao on 5/23/20.
+//  Copyright © 2020 OpenHacks Project. All rights reserved.
+//
+
+import Foundation
+import UIKit
+struct Colors{
+    static let red = UIColor(red: 255.0/255.0, green: 115.0/255.0, blue: 115.0/255.0, alpha: 1.0)
+    static let blue = UIColor(red: 74.0/255.0, green: 144.0/255.0, blue: 228.0/255.0, alpha: 1.0)
+}
---
+++ OpenHacksProject/OpenHacksProject/Gradient.swift
@@ -0,0 +1,23 @@
+//
+//  Gradient.swift
+//  OpenHacksProject
+//
+//  Created by Rahul Rao on 5/23/20.
+//  Copyright © 2020 OpenHacks Project. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView{
+    func setGradientBackground(colorOne: UIColor, colortwo: UIColor){
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colortwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x : 1.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x:0.0, y:0.0)
+        layer.insertSublayer(gradientLayer, at:0)
+    }
+}
+
