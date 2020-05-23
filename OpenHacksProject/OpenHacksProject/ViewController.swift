6f2b5b9e6264baffe4c17be32c2df97ebf495b2c
Rahul <sdrahul.rao@gmail.com>
2020-05-23 14:17:57 -0700

    c

--- OpenHacksProject/OpenHacksProject/ViewController.swift
+++ OpenHacksProject/OpenHacksProject/ViewController.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var myView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        myButton.layer.cornerRadius = myButton.frame.size.height/2
+        myButton.layer.masksToBounds = true
+        
+        view.setGradientBackground(colorOne: Colors.blue, colortwo: Colors.red)
+    }
+}
