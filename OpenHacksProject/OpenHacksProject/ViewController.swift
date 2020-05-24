5bb61f9f6f6d4e99852b77711002edac0002d655
Ryker D3M!G0D <54491562+RykerDemigod@users.noreply.github.com>
2020-05-23 19:47:12 -0700

    Update ViewController.swift
    sadaf

--- OpenHacksProject/OpenHacksProject/ViewController.swift
+++ OpenHacksProject/OpenHacksProject/ViewController.swift
@@ -10,28 +10,91 @@ import CoreLocation
 import Foundation
 import UIKit
 import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
     class ViewController: UIViewController {
+        
+        var db: Firestore!
+        
         @IBOutlet weak var backgroundGradientView: UIView!
+        
+        
+        
         var locationManager = CLLocationManager()
         override func viewDidLoad() {
-           let locationManager = CLLocationManager()
            super.viewDidLoad()
-           locationManager.requestWhenInUseAuthorization()
-           var currentLoc: CLLocation!
-           if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
-           CLLocationManager.authorizationStatus() == .authorizedAlways) {
-              currentLoc = locationManager.location
-              print(currentLoc.coordinate.latitude)
-              print(currentLoc.coordinate.longitude)
-           }
-            let gradientLayer = CAGradientLayer()
-            gradientLayer.frame = view.bounds
-            gradientLayer.colors = [#colorLiteral(red: 0, green: 0.5725490196, blue: 0.2705882353, alpha: 1).cgColor, UIColor(red: 252/255, green: 238/255, blue: 33/255, alpha: 1).cgColor]
-            gradientLayer.shouldRasterize = true
-            backgroundGradientView.layer.addSublayer(gradientLayer)
+
+           // [START setup]
+           let settings = FirestoreSettings()
+
+           Firestore.firestore().settings = settings
+           db = Firestore.firestore()
+           // [END setup]
         }
         
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+        }
+        
+        @IBAction func didTouchSmokeTestButton(_ sender: AnyObject) {
+            // Quickstar
+            addLoc()
+            getCollection()
+            
+        }
+        
+        private func addLoc(){
+            let locationManager = CLLocationManager()
+            super.viewDidLoad()
+            locationManager.requestAlwaysAuthorization()
+            var currentLoc: CLLocation!
+            if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
+            CLLocationManager.authorizationStatus() == .authorizedAlways) {
+               currentLoc = locationManager.location
+               print(currentLoc.coordinate.latitude)
+               print(currentLoc.coordinate.longitude)
+            }
+             var ref: DocumentReference? = nil
+             ref = db.collection("users").addDocument(data: [
+                 "location": [currentLoc.coordinate.latitude, currentLoc.coordinate.longitude],
+             ]) { err in
+                 if let err = err {
+                     print("Error adding document: \(err)")
+                 } else {
+                     print("Document added with ID: \(ref!.documentID)")
+                 }
+             }
+        }
+        
+        private func getCollection() {
+            // [START get_collection]
+            db.collection("users").getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        print("\(document.documentID) => \(document.data())")
+                    }
+                }
+            }
+            // [END get_collection]
+        }
+        
+       // private func checkvi(){
+            
+       // }
+        
+        func vibrateTime(dist: Float) -> Float {
+            if (dist>=12){
+                return 0
+            }
+            return Float(pow(Double(1.2),Double(dist)))/6
+        }
+
+        func distBetween(xone:Float, yone:Float, xtwo:Float, ytwo:Float) -> Float{
+            return sqrtf((yone-ytwo)*(yone-ytwo)-(xone-xtwo)*(xone-xtwo))
+        }
         
         override var shouldAutorotate: Bool {
             return false
