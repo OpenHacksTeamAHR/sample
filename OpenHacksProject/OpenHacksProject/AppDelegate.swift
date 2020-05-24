811d182e8bd34af7c82f075f1e94c1f08c7db775
Ryker D3M!G0D <54491562+RykerDemigod@users.noreply.github.com>
2020-05-23 22:40:09 -0700

    Update AppDelegate.swift
    asd

--- OpenHacksProject/OpenHacksProject/AppDelegate.swift
+++ OpenHacksProject/OpenHacksProject/AppDelegate.swift
@@ -12,13 +12,41 @@ import Firebase
 import FirebaseFirestore
 import GoogleSignIn
 
+
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+      // ...
+      if let error = error {
+        // ...
+        print(error)
+        return
+      }
 
+      guard let authentication = user.authentication else { return }
+      let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+                                                        accessToken: authentication.accessToken)
+        print(credential)
+      // ...
+    }
 
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+        // Perform any operations when the user disconnects from app here.
+        // ...
+    }
+    
+    @available(iOS 9.0, *)
+    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any])
+      -> Bool {
+      return GIDSignIn.sharedInstance().handle(url)
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         Radar.initialize(publishableKey: "prj_live_pk_30f7f876bf55bd982f09410b6fb608c9184955b0")
         FirebaseApp.configure()
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance().delegate = self
         let db = Firestore.firestore()
         print(db)
         let locManager = CLLocationManager()
